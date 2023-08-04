83297063fbe901e8b5ba4960f39438a2e0136d33
Fitri Aulia <fitrhmwti@gmail.com>
2023-08-04 20:49:22 +0700

    first commit

---
+++ src/main/java/com/juaracoding/DemoQA.java
@@ -0,0 +1,39 @@
+package com.juaracoding;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoQA {
+    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver","C:\\MyTools\\chromedriver-win64\\chromedriver.exe");
+        String path = "C:\\MyTools\\chromedriver-win64\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", path);
+        WebDriver driver = new ChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+//        driver.manage()window().maximize();
+        driver.get("https://demoqa.com/text-box");
+//        driver.manage()window().fullscree();
+        System.out.println("Open Browser, Open URL");
+
+        driver.findElement(By.id("userName")).sendKeys("JuaraCoding");
+        driver.findElement(By.id("userEmail")).sendKeys("info@juaracoding.com");
+        driver.findElement(By.id("currentAddress")).sendKeys("Jakarta");
+        driver.findElement(By.id("permanentAddress")).sendKeys("Jakarta");
+        js.executeScript("window.scrollBy(0,500)");
+        driver.findElement(By.id("submit")).click();
+
+        delay();
+        driver.quit();
+        System.out.println("Quit Browser");
+    }
+
+    static void delay(){
+        try {
+            Thread.sleep(5000); // delay 5 detik
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
